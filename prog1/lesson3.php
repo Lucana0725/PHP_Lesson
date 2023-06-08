a639ee18a62ccf8da8ed79db0dad95fb74448bee
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 00:32:35 +0900

    [Finish]問題3の回答

--- prog1/lesson3.php
+++ prog1/lesson3.php
@@ -1,11 +1,13 @@
 <?php
 // 初期記述
 $price = 1000;
-$taxRate = 0.008;
+$taxRate = 0.08;
 echo '変数$priceの値：' . $price . PHP_EOL;
 echo '変数$taxRateの値：' . $taxRate . PHP_EOL;
 echo '----------' . PHP_EOL;
 // ここまで
 
-
+// $priceと$taxRateを使って"税込価格は**円です"と出力せよ
+$taxInPrice = $price + $price * $taxRate;
+echo "税込価格は{$taxInPrice}円です";
 ?>
