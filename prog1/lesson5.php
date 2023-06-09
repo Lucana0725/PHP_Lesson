0e4b13d5b8fa65d13426be3bcd7003054a843afd
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 13:53:55 +0900

    [Finish]道場1-11回答

--- prog1/lesson5.php
+++ prog1/lesson5.php
@@ -8,15 +8,22 @@ $menus = array(
 // ここまで
 
 // 問題：全てのメニューの合計金額を算出せよ
+// 問題：メニューの中で最高金額の商品を「**が最高価格で、++円です」と表示せよ
 
 
-$totalPrice = 0;
+$totalPrice = 0;  // トータル金額を保持
+$maxPrice = 0;  // 最高価格を保持
+$maxPriceMenu;  // 最高価格の商品を保持(ここでは初期化はせず定義するだけ)
 foreach ($menus as $menu) {
+  if ($menu['price'] > $maxPrice) {
+    $maxPrice = $menu['price'];
+    $maxPriceMenu = $menu['name'];
+  }
   echo "{$menu['name']}は{$menu['price']}円です" . PHP_EOL;
   $totalPrice += $menu['price'];
 }
 
 echo "合計金額は{$totalPrice}円です" . PHP_EOL;
-
+echo "{$maxPriceMenu}が最高価格で{$maxPrice}円です" . PHP_EOL;
 
 ?>
