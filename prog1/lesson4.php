0d93502d55a2e4a354cf57926acc5c3fdcd51232
Lucana <k.ark.scarlet72@gmail.com>
2023-06-09 13:36:07 +0900

    [Finish]道場1-9回答

---
+++ prog1/lesson4.php
@@ -0,0 +1,17 @@
+<?php
+// 初期記述
+$menus = array(
+  array("name" => "カレー", "price" => "900"),
+  array("name" => "パスタ", "price" => "1200"),
+  array("name" => "コーヒー", "price" => "600"),
+);
+// ここまで
+
+// $namesを使って**は++円です と一件ずつ改行して順番どおりに表示せよ
+
+foreach ($menus as $menu) {
+  echo "{$menu['name']}は{$menu['price']}円です" . PHP_EOL;
+}
+
+
+?>
