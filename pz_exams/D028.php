a5c6680b05586bf7a51be462d874c9fe420bf6da
Lucana <k.ark.scarlet72@gmail.com>
2023-07-15 21:18:25 +0900

    [Finish]一桁区切りに分割した配列を逆順表示に対応

--- pz_exams/D028.php
+++ pz_exams/D028.php
@@ -1,26 +1,31 @@
 <?php
 
-  $input = 100;
-  // $nums = array();
+  // 入力値
+  $input = 114514;
 
-  // while($input > 0) {
-  //   $nums[] = $input % 10;
-  //   $input = intval($input / 10);
-  // }
+  // 一桁区切りに分解
+  $nums = array();
 
-  // print_r($nums);
+  while($input > 0) {
+    $nums[] = $input % 10;
+    $input = intval($input / 10);
+  }
+
+  print_r($nums);
+  $reverseNums = array_reverse($nums);  // 配列$numsを逆順(元数値順)に
+  print_r($reverseNums);
 
   // // 桁数を出力
   // print_r(strlen($input));
 
-  // 桁数を出力 その2
-  $nums = array();
+  // // 桁数を出力 その2
+  // $nums = array();
 
-  while ($input > 0) {
-    $nums[] = $input % 10;
-    $input = intval($input / 10);
-  }
-  
-  echo count($nums);
+  // while ($input > 0) {
+  //   $nums[] = $input % 10;
+  //   $input = intval($input / 10);
+  // }
+
+  // echo count($nums);
 
 ?>
