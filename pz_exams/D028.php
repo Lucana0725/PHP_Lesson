a61a7555522d56d2d806ec9056dbd52d04e11e69
Lucana <k.ark.scarlet72@gmail.com>
2023-07-15 21:12:25 +0900

    [Finish]標準入力値の数の桁数を表示するプログラムの作成

---
+++ pz_exams/D028.php
@@ -0,0 +1,16 @@
+<?php
+
+  $input = 100;
+  // $nums = array();
+
+  // while($input > 0) {
+  //   $nums[] = $input % 10;
+  //   $input = intval($input / 10);
+  // }
+
+  // print_r($nums);
+
+  // 桁数を出力
+  print_r(strlen($input));
+
+?>
