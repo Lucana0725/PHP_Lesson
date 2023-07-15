65f5df172e81c375714b148a85739f4ab2a15621
Lucana <k.ark.scarlet72@gmail.com>
2023-07-15 21:14:32 +0900

    [Finish]桁数を表示する方法の2通り目を作成

--- pz_exams/D028.php
+++ pz_exams/D028.php
@@ -10,7 +10,17 @@
 
   // print_r($nums);
 
-  // 桁数を出力
-  print_r(strlen($input));
+  // // 桁数を出力
+  // print_r(strlen($input));
+
+  // 桁数を出力 その2
+  $nums = array();
+
+  while ($input > 0) {
+    $nums[] = $input % 10;
+    $input = intval($input / 10);
+  }
+  
+  echo count($nums);
 
 ?>
