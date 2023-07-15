e6c91c8444f1804b50ac49e9de13138203614404
Lucana <k.ark.scarlet72@gmail.com>
2023-07-15 12:06:12 +0900

    [Finish]ユーザーからの入力値を標準入力として扱えるよう練習

--- pz_exams/D215.php
+++ pz_exams/D215.php
@@ -1,8 +1,25 @@
 <?php
 
-  $str = readline('パスワードを入力してください');
-  echo $str . PHP_EOL;
+  // $str = readline('パスワードを入力してください');
+  // $password = 'guitar';
+  // $str = readline("Enter your password.\n");
+  // echo $str;
+  // echo readline("Enter your password\n");
 
 
+  // $inputLine = fgets(STDIN);
+  // if ($iputLine == $password) {
+  //   echo 'Welcome!';
+  // } else {
+  //   echo 'Invalid password';
+  // }
+
+
+  echo "Enter your password\n";
+  $handle = fopen("php://stdin", "r");
+  $password = fgets($handle);
+  fclose($handle);
+
+  echo "Entered password: " . $password;
 
 ?>
