948651162f788430f61e0e29404b60cac77d89a3
Lucana <k.ark.scarlet72@gmail.com>
2023-07-18 12:30:55 +0900

    [Finish]プレイ人数と初期所有数の入力の確認

--- pz_exams/C098.php
+++ pz_exams/C098.php
@@ -1,5 +1,29 @@
 <?php
 
-  
+  function rules() {
+    echo 'プレイヤーは最初にいくらかボールを所持しており、パスを回すときは回す相手と回す個数を宣言します。' . PHP_EOL;
+    echo 'その際、宣言数がプレイヤー自身が所持している個数以下の場合は宣言数を渡せますが、宣言数を上回る場合は所持数を全て渡します。' . PHP_EOL;
+    echo '各プレイヤーは持てるボールの個数に上限はありません。';
+  }
+
+  function userInputs() {
+    echo 'まずはプレイ人数を入力してください。(テスト用に1を入力)' . PHP_EOL;
+    $input = fopen('php://stdin', 'r');
+    $numOfPlayers = intval(trim(fgets($input)));
+    fclose($input);
+
+    echo '次に各プレイヤーの所持数を入力してください。' . PHP_EOL;
+    $input = fopen('php://stdin', 'r');
+    $playerHas = intval(trim(fgets($input)));
+    fclose($input);
+    // 入力確認用
+    echo "プレイ人数：{$numOfPlayers}人" . PHP_EOL;
+    echo "初期所有数：{$playerHas}個";
+  }
+
+  echo '両隣入りのどちらかの人にボールをパスするパス回しゲームをプレイします。' . PHP_EOL;
+  rules();
+  userInputs();
+
 
 ?>
