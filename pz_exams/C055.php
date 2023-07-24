18ac88d824cb8778eac846e2e078e2c83e56ac1c
Lucana <k.ark.scarlet72@gmail.com>
2023-07-24 18:24:06 +0900

    [Finish]ユーザーからの入力値を取得する機能を実装

--- pz_exams/C055.php
+++ pz_exams/C055.php
@@ -1,5 +1,12 @@
 <?php
 
-  
+  echo '入力されたキーワードにヒットするデータを出力します。' . PHP_EOL;
+  echo '検索したいキーワードを入力してください。' . PHP_EOL;
+
+  $input = fopen('php://stdin', 'r');
+  $keyword = trim(fgets($input));
+  fclose($input);
+
+
 
 ?>
