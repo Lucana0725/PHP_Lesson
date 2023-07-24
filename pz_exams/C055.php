8ceb523313d15e3a3478777542d92345a5864465
Lucana <k.ark.scarlet72@gmail.com>
2023-07-24 18:31:33 +0900

    [Finish]比較対象のデータを用意

--- pz_exams/C055.php
+++ pz_exams/C055.php
@@ -1,12 +1,16 @@
 <?php
 
+// 用意されているデータ
+  $data = array('paizaonlinehackathon');
+  // $data = array('pizza', 'paiza', 'aizu', 'ai', 'sai');
+  // $data = array('safe', 'normal', 'test');
+
   echo '入力されたキーワードにヒットするデータを出力します。' . PHP_EOL;
   echo '検索したいキーワードを入力してください。' . PHP_EOL;
 
   $input = fopen('php://stdin', 'r');
   $keyword = trim(fgets($input));
   fclose($input);
-
 
 
 ?>
