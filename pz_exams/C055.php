5cbe5f8dfd53b013be0d0573e83afdab43d384f4
Lucana <k.ark.scarlet72@gmail.com>
2023-07-24 18:39:58 +0900

    [Finish]foreachのループを使い、の各要素にが含まれているか否かを判断し、該当する場合はへ追加する処理を実装

--- pz_exams/C055.php
+++ pz_exams/C055.php
@@ -12,5 +12,18 @@
   $keyword = trim(fgets($input));
   fclose($input);
 
+  $collects = [];  // 入力値の文字列が含まれている文字列を追加する空配列
+
+  // 用意されたデータに対して、$keywordが含まれているかを調べるためのループ処理
+  foreach($data as $datum) {
+    $result = mb_strpos($datum, $keyword);  // mb_strpos()を使って、$dataの各要素に$keywordが含まれているかの結果を保持
+
+    // $dataの各要素に$keywordが含まれていれば、$collectsへ追加
+    if($result !== false) {
+      $collects[] = $datum;
+    }
+  }
+
+  
 
 ?>
