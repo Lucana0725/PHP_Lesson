a8a4f5ce5dba060ad6e9044742738a765baee694
Lucana <k.ark.scarlet72@gmail.com>
2023-07-30 15:57:53 +0900

    [Update]どのくじにどれだけ当選番号があるかを表示するようにして視認性を向上、当選番号がない場合は存在しないと表示するように変更

--- pz_exams/C023.php
+++ pz_exams/C023.php
@@ -4,7 +4,7 @@
   echo '抽選では6つの当選番号が発表され、購入したクジの数字と一致していた数字の数に応じて賞金が当たります。' . PHP_EOL;
   echo 'あなたは先日N枚のくじを購入しました。' . PHP_EOL;
   echo '当選番号と、あなたが購入したくじの番号が与えられますので、当選番号と一致した数字の数について調べるプログラムを書いてください。' . PHP_EOL;
-  echo '例' . PHP_EOL;
+  // echo '例' . PHP_EOL;
   // echo '当選番号：1 2 3 4 5 6' . PHP_EOL;
   // echo 'あなたの所持'
 
@@ -30,7 +30,8 @@
   $yourLotos = array(
     ['36 70 1 72 54 82'],
     ['36 2 40 12 3 58'],
-    ['25 11 90 57 85 99']
+    ['25 11 90 57 85 99'],
+    ['60', '61', '62', '63', '64', '65']
 
   );
 
@@ -61,7 +62,17 @@
     $matchCounts[] = $existCount;
   }
 
-  print_r($matchCounts);
+  // print_r($matchCounts);
+  foreach($matchCounts as $key => $value) {
+    $displayKey = $key + 1;
+    if($value != 0) {
+      // echo "{$key}枚目のくじには{$value}個の当選番号が存在します" . PHP_EOL;
+      // echo ($key+1) . '枚目のくじには' . $value . '個の当選番号が存在します' . PHP_EOL;
+      echo "{$displayKey}枚目のくじには{$value}個の当選番号が存在します" . PHP_EOL;
+    } else {
+      echo "{$displayKey}枚目のくじには当選番号はありません" . PHP_EOL;
+    }
+  }
 
 
 ?>
