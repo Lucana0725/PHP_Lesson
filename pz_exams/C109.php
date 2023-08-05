3330c68e49d1bc85c87f008ce587adbb8bae9a76
Lucana <k.ark.scarlet72@gmail.com>
2023-08-06 00:08:15 +0900

    [Finish]問題文と問題のユーザーIDを定義

--- pz_exams/C109.php
+++ pz_exams/C109.php
@@ -1,5 +1,13 @@
 <?php
+$userIDs = ['pays1', 'kirishima813', 'pizza99', 'sushi100', 'beef1001'];
 
+echo 'ユーザーIDが「ユーザーネーム + 通し番号」となるSNSがあります。' . PHP_EOL;
+echo 'ユーザーIDが与えられますので、ユーザーIDを通し番号の若い順に並べてください。' . PHP_EOL;
+
+echo 'ユーザーID：' . PHP_EOL;
+foreach($userIDs as $userID) {
+  echo $userID . PHP_EOL;
+}
 
 
 ?>
